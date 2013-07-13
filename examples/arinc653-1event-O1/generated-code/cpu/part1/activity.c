fe3f0e49fee009ff67b89eb8a8d45b30b1f6baa1
Marco Ziccardi <marco.ziccard@gmail.com>
2013-07-13 10:41:34 +0200

    Formatted application's prints

--- examples/arinc653-1event-O1/generated-code/cpu/part1/activity.c
+++ examples/arinc653-1event-O1/generated-code/cpu/part1/activity.c
@@ -17,15 +17,11 @@ extern PROCESS_ID_TYPE arinc_threads[POK_CONFIG_NB_THREADS];
 void* thr1_1_job ()
 {
 	RETURN_CODE_TYPE ret;
+	printf("\t[Thread 1] Partition n.1\n");
 	while (1)
 	{
-		printf("Partition n. 1 - Thread n.1\n");
+		printf("\t[Thread 1] Do something\n");
 
-		/***************************************************************/
-		/* Message processing code should be placed here               */
-		/***************************************************************/
-
-		printf("1: -- do something\n\t\t1 :: Waiting on an event\n\n");
 		RESUME (arinc_threads[2], &(ret));
 		PERIODIC_WAIT (&(ret));
 	}
@@ -36,37 +32,36 @@ void* thr1_1bis_job()
 	int flag = 0;
 	RETURN_CODE_TYPE ret;
 	SYSTEM_TIME_TYPE time = APERIODIC_PROCESS;
-	printf("	Partition n. 1 - Thread n.2\n");
-			printf ("\t2: Sporadic (wait evt)\n");
+	printf("\t[Thread 2] Partition n.1\n");
+	printf("\t[Thread 2] Sporadic (wait evt)\n");
+
 	while (1)
 	{
 		if (flag){
-			printf ("\t2: Th self-suspending.........zzzz\n");
+			printf ("\t[Thread 2] Thread self-suspending\n");
 		} else {
 			flag=1;
 		}
+
 		SUSPEND_SELF (time, &ret);
-		printf ("\t2: Resume - Going to WAIT_EVENT \n");
+		printf ("\t[Thread 2] Resumed, going to WAIT_EVENT \n");
 		WAIT_EVENT (pok_arinc653_events_ids[0], 0, &(ret));
-		printf ("\t2: Event %i is UP\n", (unsigned int) pok_arinc653_events_ids[0]);
+		printf ("\t[Thread 2] Event %i is UP\n", (unsigned int) pok_arinc653_events_ids[0]);
 	}
 }
 
 void* thr1_3_job ()
 {
 	RETURN_CODE_TYPE ret;
+	printf("\t[Thread 3] Partition n.1\n");
+
 	while (1)
 	{
-		printf("Partition n. 1 - Thread n.3\n");
+		printf("\t[Thread 3] Do something\n");
 
-		/***************************************************************/
-		/* Message processing code should be placed here               */
-		/***************************************************************/
-
-		printf(" 3: -- do something\n");
-		printf(" 3: Setting an event UP\n\n");
+		printf("\t[Thread 3] Setting event UP\n");
 		SET_EVENT (pok_arinc653_events_ids[0], &(ret));
-		printf(" 3: Event SET -- Resume sporadic\n\n");
+		printf("\t[Thread 3] Event SET, resuming sporadic\n");
 		RESUME (arinc_threads[4], &(ret));
 		PERIODIC_WAIT (&(ret));
 	}
@@ -77,14 +72,15 @@ void* thr1_3bis_job()
 	int flag = 0;
 	RETURN_CODE_TYPE ret;
 	SYSTEM_TIME_TYPE time = APERIODIC_PROCESS;
-	printf("\t4: Sporadic (after set evt)\n");
+	printf("\t[Thread 4] Partition n.1\n");
+	printf("\t[Thread 4] Sporadic (after set evt)\n");
 	while (1)
 	{
 		if (flag){
-			printf("\t4: Resume - Continuation after event is set\n\n");
+			printf("\t[Thread 4] Resumed, continuation after event is set\n");
 			RESET_EVENT (pok_arinc653_events_ids[0], &(ret));
-			printf("\t4: Event %i is DOWN [%i]\n", (unsigned int) pok_arinc653_events_ids[0], ret);
-			printf("\t4: Th self-suspending.........zzzz\n");
+			printf("\t[Thread 4] Event %i is DOWN [%i]\n", (unsigned int) pok_arinc653_events_ids[0], ret);
+			printf("\t[Thread 4] Thread self-suspending\n");
 		} else {
 			flag=1;
 		}
