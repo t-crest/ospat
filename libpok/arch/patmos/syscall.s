8eaea7c6d9a7b7d02a594fe5fbc101eb9423b3cd
mziccard <marco.ziccard@gmail.com>
2014-04-17 13:09:20 +0200

    Changed implementation of system call to user traps

---
+++ libpok/arch/patmos/syscall.s
@@ -0,0 +1,103 @@
+/*  
+ * 		Copyright (c) 2007-2009 POK team
+ *
+ *		Redistribution and use in source and binary forms, with or without
+ *		modification, are permitted provided that the following conditions
+ *		are met:
+ *
+ *			* Redistributions of source code must retain the above copyright
+ *			  notice, this list of conditions and the following disclaimer.
+ *			* Redistributions in binary form must reproduce the above
+ *			  copyright notice, this list of conditions and the following
+ *			  disclaimer in the documentation and/or other materials
+ *			  provided with the distribution.
+ *			* Neither the name of the POK Team nor the names of its main
+ *			  author (Julien Delange) or its contributors may be used to
+ *			  endorse or promote products derived from this software
+ *			  without specific prior written permission.
+ *
+ *		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *		AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *		LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *		FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *		COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *		INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *		BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *		LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *		CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *		LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *		ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *		POSSIBILITY OF SUCH DAMAGE.
+ */
+
+ 		.globl 		pok_syscall2
+		.type 		pok_syscall2,@function
+		.size 		pok_syscall2, .Ltmp2-pok_syscall2
+		.fstart		pok_syscall2, .Ltmp2-pok_syscall2, 4
+pok_syscall2:
+		trap 3
+		ret
+		nop
+		nop
+		nop
+.Ltmp2:
+
+ 		.globl 		pok_syscall3
+		.type 		pok_syscall3,@function
+		.size 		pok_syscall3, .Ltmp3-pok_syscall3
+		.fstart		pok_syscall3, .Ltmp3-pok_syscall3, 4
+pok_syscall3:
+		trap 3
+		ret
+		nop
+		nop
+		nop
+.Ltmp3:
+
+ 		.globl 		pok_syscall4
+		.type 		pok_syscall4,@function
+		.size 		pok_syscall4, .Ltmp4-pok_syscall4
+		.fstart		pok_syscall4, .Ltmp4-pok_syscall4, 4
+pok_syscall4:
+		trap 3
+		ret
+		nop
+		nop
+		nop
+.Ltmp4:
+
+ 		.globl 		pok_syscall5
+		.type 		pok_syscall5,@function
+		.size 		pok_syscall5, .Ltmp2-pok_syscall5
+		.fstart		pok_syscall5, .Ltmp2-pok_syscall5, 4
+pok_syscall5:
+		trap 3
+		ret
+		nop
+		nop
+		nop
+.Ltmp5:
+
+ 		.globl 		pok_syscall6
+		.type 		pok_syscall6,@function
+		.size 		pok_syscall6, .Ltmp6-pok_syscall6
+		.fstart		pok_syscall6, .Ltmp6-pok_syscall6, 4
+pok_syscall6:
+		trap 3
+		ret
+		nop
+		nop
+		nop
+.Ltmp6:
+
+ 		.globl 		pok_syscall7
+		.type 		pok_syscall7,@function
+		.size 		pok_syscall7, .Ltmp7-pok_syscall7
+		.fstart		pok_syscall7, .Ltmp7-pok_syscall7, 4
+pok_syscall7:
+		trap 3
+		ret
+		nop
+		nop
+		nop
+.Ltmp7:
