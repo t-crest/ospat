e6c0623b4af39970d1718c96f97dcc357d3c325b
Marco Ziccardi <marco.ziccard@gmail.com>
2014-07-10 15:34:59 +0200

    Modified elf2uart utility to print a textfile for each loadable section and a textfile with the entry point of the elf file submitted

--- elf2uart/src/elf2uart.cc
+++ elf2uart/src/elf2uart.cc
@@ -18,6 +18,7 @@
 
 // Command line options
 #include <boost/program_options.hpp>
+#include <boost/lexical_cast.hpp>
 
 // Collect load segment headers
 #include <vector>
@@ -30,115 +31,131 @@ static inline uint32_t toBigEndian(uint32_t value)
 						 ((value & 0xFF) << 24);
 }
 
-static void streamelf(const char* elf_filename, const char* uart_filename)
+static void streamelf(const std::vector<std::string> filenames, const char* uart_filename, const int file_number)
 {
-  // check libelf version
-  elf_version(EV_CURRENT);
+  FILE* entry_file;
+  std::string fn = uart_filename;
+  fn = fn + "_entry";
+  entry_file = fopen(fn.c_str(), "w");
+  assert(entry_file);
 
-  // open elf binary
-  int elf_file = open(elf_filename, O_RDONLY, 0);
-  assert(elf_file > 0);
+  for (int j = 0; j < file_number; j++) {
 
-	FILE* uart_file;
-  uart_file = fopen(uart_filename, "w");
-	assert(uart_file);
+    const char* elf_filename = filenames[j].c_str();
+    // check libelf version
+    elf_version(EV_CURRENT);
 
-  Elf *elf = elf_begin(elf_file, ELF_C_READ, NULL);
-  assert(elf);
+    // open elf binary
+    int elf_file = open(elf_filename, O_RDONLY, 0);
+    assert(elf_file > 0);
 
-  // check file kind
-  Elf_Kind ek = elf_kind(elf);
-  assert(ek == ELF_K_ELF);
+    Elf *elf = elf_begin(elf_file, ELF_C_READ, NULL);
+    assert(elf);
 
-  // check class
-  int ec = gelf_getclass(elf);
-  assert(ec == ELFCLASS32);
+    // check file kind
+    Elf_Kind ek = elf_kind(elf);
+    assert(ek == ELF_K_ELF);
 
-  // get elf header
-  GElf_Ehdr hdr;
-  GElf_Ehdr *tmphdr = gelf_getehdr(elf, &hdr);
-  assert(tmphdr);
+    // check class
+    int ec = gelf_getclass(elf);
+    assert(ec == ELFCLASS32);
 
-  // get program headers
-  size_t n, i;
-  int ntmp = elf_getphdrnum(elf, &n);
-  assert(ntmp == 0);
+    // get elf header
+    GElf_Ehdr hdr;
+    GElf_Ehdr *tmphdr = gelf_getehdr(elf, &hdr);
+    assert(tmphdr);
 
-  uint32_t entry = hdr.e_entry;
-  std::cout << "[elf2uart] " << std::left << std::setw(10) << "ENTRY" << entry << std::endl;
-	
-	uint32_t big_entry = toBigEndian(entry);
-	fwrite(&big_entry, 4, 1, uart_file);
-  
-	std::vector<GElf_Phdr> load_segments;
+    // get program headers
+    size_t n, i;
+    int ntmp = elf_getphdrnum(elf, &n);
+    assert(ntmp == 0);
 
-  for(i = 0; i < n; i++)
-  {
-    // get program header
-    GElf_Phdr phdr;
-    GElf_Phdr *phdrtmp = gelf_getphdr(elf, i, &phdr);
-    assert(phdrtmp);
+    uint32_t entry = hdr.e_entry;
+    uint32_t* entry_ptr = (uint32_t*) entry;
+    uint32_t big_entry = toBigEndian(entry);
+    std::cout << "[elf2uart] " << std::left << std::setw(10) << "ENTRY" << (void*)entry << std::endl;
+  	
+  	fprintf(entry_file, "%p", entry_ptr);
+    
+  	std::vector<GElf_Phdr> load_segments;
 
-    if (phdr.p_type == PT_LOAD)
+    for(i = 0; i < n; i++)
     {
-			load_segments.push_back(phdr);
-		}
-	}
+      // get program header
+      GElf_Phdr phdr;
+      GElf_Phdr *phdrtmp = gelf_getphdr(elf, i, &phdr);
+      assert(phdrtmp);
 
-	int segments_number = load_segments.size();
-	std::cout << "[elf2uart] " << std::left << std::setw(10) << "SEGMENTS" << segments_number << std::endl;
+      if (phdr.p_type == PT_LOAD)
+      {
+  			load_segments.push_back(phdr);
+  		}
+  	}
 
-	uint32_t big_segments_number = toBigEndian(segments_number);
-	fwrite(&big_segments_number, 4, 1, uart_file);
+  	int segments_number = load_segments.size();
+  	std::cout << "[elf2uart] " << std::left << std::setw(10) << "SEGMENTS" << segments_number << std::endl;
 
-	std::cout << "[elf2uart] " << std::left << std::setw(10) << "START" 
-																					<< std::setw(15) << "FILE SIZE" 
-																					<< std::setw(10) << "TOTAL SIZE" << std::endl;
+  	uint32_t big_segments_number = toBigEndian(segments_number);
+  	//fwrite(&big_segments_number, 4, 1, uart_file);
 
-	for(std::vector<GElf_Phdr>::iterator it = load_segments.begin(); it != load_segments.end(); ++it) 
-	{
-		GElf_Phdr phdr = *it;
+  	std::cout << "[elf2uart] " << std::left << std::setw(10) << "START" 
+  																					<< std::setw(15) << "FILE SIZE" 
+  																					<< std::setw(10) << "TOTAL SIZE" << std::endl;
 
-  	// some assertions
-  	assert(phdr.p_vaddr == phdr.p_paddr);
-  	assert(phdr.p_filesz <= phdr.p_memsz);
+    int segment_index = 0;
+  	for(std::vector<GElf_Phdr>::iterator it = load_segments.begin(); it != load_segments.end(); ++it) 
+  	{
 
-  	// allocate buffer
-  	char *buf = (char*)malloc(phdr.p_filesz);
-  	assert(buf);
+      FILE* uart_file;
+      std::string fn = uart_filename;
+      fn = fn + "_sec" + boost::lexical_cast<std::string>(segment_index);
+      uart_file = fopen(fn.c_str(), "w");
+      assert(uart_file);
 
-  	// copy from the buffer into the main memory
-  	lseek(elf_file, phdr.p_offset, SEEK_SET);
-  	read(elf_file, buf, phdr.p_filesz);
+      segment_index++;
+  		GElf_Phdr phdr = *it;
 
-  	uint32_t start_offset = phdr.p_vaddr;
-  	uint32_t size = phdr.p_filesz;
-  	uint32_t total_size = phdr.p_memsz;
+    	// some assertions
+    	assert(phdr.p_vaddr == phdr.p_paddr);
+    	assert(phdr.p_filesz <= phdr.p_memsz);
 
-		std::cout << "[elf2uart] " << std::left << std::setw(10) << start_offset
-																						<< std::setw(15) << size
-																						<< std::setw(10) << total_size << "\n";
+    	// allocate buffer
+    	char *buf = (char*)malloc(phdr.p_filesz);
+    	assert(buf);
 
-		uint32_t big_start_offset = toBigEndian(start_offset);
-		uint32_t big_size = toBigEndian(size);
+    	// copy from the buffer into the main memory
+    	lseek(elf_file, phdr.p_offset, SEEK_SET);
+    	read(elf_file, buf, phdr.p_filesz);
 
-		fwrite(&big_start_offset, sizeof start_offset, 1, uart_file);
-		fwrite(&big_size, sizeof size, 1, uart_file);
-        
-    int j;        
-    for(j=0; j < size; j++)
-    {
-			fwrite(&(buf[j]), 1, 1, uart_file);
-    }
+    	uint32_t start_offset = phdr.p_vaddr;
+    	uint32_t size = phdr.p_filesz;
+    	uint32_t total_size = phdr.p_memsz;
 
-	  free(buf);
-	}
+  		std::cout << "[elf2uart] " << std::left << std::setw(10) << (void*) start_offset
+  																						<< std::setw(15) << size
+  																						<< std::setw(10) << total_size << "\n";
 
-  elf_end(elf);
-  close(elf_file);
-  //uart_file.close();
-	fclose(uart_file);
+  		uint32_t big_start_offset = toBigEndian(start_offset);
+  		uint32_t big_size = toBigEndian(size);
 
+  		//fwrite(&big_start_offset, sizeof start_offset, 1, uart_file);
+  		//fwrite(&big_size, sizeof size, 1, uart_file);
+          
+      int j;        
+      for(j=0; j < size; j++)
+      {
+  			fwrite(&(buf[j]), 1, 1, uart_file);
+      }
+
+  	  free(buf);
+      fclose(uart_file);
+  	}
+
+    elf_end(elf);
+    close(elf_file);
+    //uart_file.close();
+  }
+  fclose(entry_file);
 }
 
 int main(int argc, char* argv[]) {
@@ -147,11 +164,11 @@ int main(int argc, char* argv[]) {
   boost::program_options::options_description generic_options("Generic options");
   generic_options.add_options()
     ("help,h", "produce help message")
-    ("binary,b", boost::program_options::value<std::string>()->required(), "elf-executable file (stdin: -)")
+    ("binaries,b", boost::program_options::value< std::vector<std::string> >()->required(), "elf-executable file (stdin: -)")
     ("output,o", boost::program_options::value<std::string>()->default_value("UART"), "output UART serialization in file");
 
   boost::program_options::positional_options_description pos;
-  pos.add("binary", 1);
+  pos.add("binaries", 1);
 
 	boost::program_options::options_description cmdline_options;
   cmdline_options.add(generic_options);
@@ -177,10 +194,10 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-	std::string binary(vm["binary"].as<std::string>());
+	std::vector<std::string> binaries(vm["binaries"].as< std::vector<std::string> >());
 	std::string output(vm["output"].as<std::string>());
 	
-  streamelf(binary.c_str(), output.c_str());  
+  streamelf(binaries, output.c_str(), binaries.size());  
 
   return 0;  
 }
